1253b929764c25e0a6b0bf608e92391bfbf84159
Eric Davis <edavis@littlestreamsoftware.com>
2009-10-02 13:01:36 -0700

    Some method documentation

--- lib/form_protector/view_helpers.rb
+++ lib/form_protector/view_helpers.rb
@@ -1,9 +1,12 @@
 module FormProtector
   module ViewHelpers
+    # Include the required Javascript libraries
     def javascript_include_form_protector
       javascript_include_tag 'form_protector/FormProtector'
     end
-    
+
+    # Protect the form with the <tt>dom_id</tt> by prompting the user
+    # before they nagivate away from the page.
     def protect_form(dom_id, options={})
       javascript_tag("var #{dom_id}_protector = new FormProtector('#{dom_id}')")
     end
